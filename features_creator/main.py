9b413ecb102cdc23f2441fe7ab74487dfd0cd0fb
Federico Torres <federicotorres2109@gmail.com>
2022-03-16 12:31:35 -0600

    Feat: Reutilization of Noise recognition

--- features_creator/main.py
+++ features_creator/main.py
@@ -1,12 +1,16 @@
-import tkinter as tk
-from tkinter import messagebox
-from audio_tools import record,save_record
-
-window = tk.Tk()
-
-def program_manager():
-    window.geometry("500x250") 
-    window.mainloop()
+import streamlit as st
+from audio_tools import record, save_record
 
 
-program_manager()
+
+st.markdown("**Recording features**")
+stop = st.button(f"Click to stop recording")
+start = st.b(f"Click to record")
+counter = 0
+st.text("It is going to be started the microphone")
+while not stop:
+    myrecording = record(10,44100)
+    path_myrecording = f"./own_features/feature({counter})"
+    save_record(path_myrecording, myrecording,44100)
+    counter += 1
+    
